3c6ef34aa30350f44bf2cecbce63112dfd40c879
webdesigngr-vasstos <webdesigngr@vasstos.com>
2025-12-23 12:04:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Garante que o script funcione em qualquer subpasta do GitHub Pages
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
-});
+  base: '/Chatbot-vasstos/', // Deve ser igual ao nome do repositório no GitHub
+})
+// export default defineConfig({
+//  plugins: [react()],
+//  base: './', // Garante que o script funcione em qualquer subpasta do GitHub Pages
+//  build: {
+ //   outDir: 'dist',
+ //   rollupOptions: {
+  //    output: {
+ //       entryFileNames: `assets/[name].js`,
+ //       chunkFileNames: `assets/[name].js`,
+//        assetFileNames: `assets/[name].[ext]`
+//      }
+//    }
+//  }
+// });
